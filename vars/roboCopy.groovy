9cd3282f8ab4f55411387ff8e2fee9bfd43099e3
Michael Delva <michael@emidee.net>
2020-07-17 09:25:57 +0200

    Added roboCopy

---
+++ vars/roboCopy.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/groovy
+
+def call( source, destination, arguments ) {
+    def status = bat returnStatus: true, script: "robocopy.exe ${source} ${destination} ${arguments}"
+
+    if (status < 0 || status > 3) {
+        error( "ROBOCOPY failed" )
+    }
+}
