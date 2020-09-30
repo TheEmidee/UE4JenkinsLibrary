8d68adb06300e6f258395a0903b3523d778deb02
Michael Delva <michael@emidee.net>
2020-09-30 19:08:06 +0200

    Removed stage from runBuildGraph

--- vars/runBuildGraph.groovy
+++ vars/runBuildGraph.groovy
@@ -6,8 +6,6 @@ def call( List tasks, Map parameters = [:], String default_arguments = "" ) {
     UE4.initialize( env.PROJECT_NAME, env.WORKSPACE, env.UE4_ROOT_WINDOWS, default_arguments )
 
     tasks.each {
-        stage( it ) {
-            UE4.runBuildGraph( env.UE4_RELATIVE_BUILD_GRAPH_PATH, it, parameters )
-        }
+        UE4.runBuildGraph( env.UE4_RELATIVE_BUILD_GRAPH_PATH, it, parameters )
     }
 }
