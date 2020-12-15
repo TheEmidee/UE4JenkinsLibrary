5bd011e6e7d634764ed9b13964ca4f043cda4b20
michael.delva <michael@emidee.net>
2020-12-15 14:27:17 +0100

    Added function to get the email of the last committer of a PR

--- src/unreal/utils.groovy
+++ src/unreal/utils.groovy
@@ -89,6 +89,30 @@ def getGitHubPRInfos( github_token ) {
     return [ json.title, json.body, json.user.login, json.user.avatar_url ]
 }
 
+def getGitHubPRLastCommitterEmail( github_token ) {
+    branch_name = env.GIT_BRANCH
+    if (env.BRANCH_NAME != null) {
+        branch_name = env.BRANCH_NAME
+    }
+
+    pr_id = branch_name.substring( 3 )
+    echo "PR ID : ${pr_id}"
+
+    String url = "https://api.github.com/repos/FishingCactus/${env.PROJECT_NAME}/pulls/${pr_id}/commits"
+    
+    def text = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
+    def json = new JsonSlurper().parseText( text )
+    def last_commit = json[ 0 ]
+
+    log.info "Last commit SHA : ${last_commit.sha}"
+
+    def last_commit_author_email = last_commit.commit.author.email
+
+    log.info "Last commit author email : ${last_commit_author_email}"
+
+    return last_commit_author_email
+}
+
 def getProjectName(def script) {
     split_result = "${script.env.JOB_NAME}".split('/')
     project_name = split_result.length > 1 ? split_result[split_result.length - 2] : split_result.max()
