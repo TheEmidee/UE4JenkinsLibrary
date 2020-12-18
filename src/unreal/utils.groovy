0d089513432da6a03dcaae5834860ec76403d859
michael.delva <michael.delva@fishingcactus.com>
2020-12-18 10:42:54 +0100

    Renamed UE4_ROOT by NODE_UE4_ROOT

--- src/unreal/utils.groovy
+++ src/unreal/utils.groovy
@@ -28,12 +28,12 @@ enum BuildConfiguration {
 def initializeNode(Script script) {
     log.info "Initialize Node"
 
-    log.info "UE4_ROOT : ${script.env.UE4_ROOT}"
-    if ( script.env.UE4_ROOT == null || script.env.UE4_ROOT.isEmpty() ) {
-        log.fatal "Missing environment variable UE4_ROOT. Add it to the node properties."
+    log.info "UE4_ROOT : ${script.env.NODE_UE4_ROOT}"
+    if ( script.env.NODE_UE4_ROOT == null || script.env.NODE_UE4_ROOT.isEmpty() ) {
+        log.fatal "Missing environment variable NODE_UE4_ROOT. Add it to the node properties."
     }
 
-    script.env.UE4_ROOT_WINDOWS = "${script.env.UE4_ROOT}\\Windows"
+    script.env.NODE_UE4_ROOT_WINDOWS = "${script.env.NODE_UE4_ROOT}\\Windows"
 
     global_workspace = new File( script.env.WORKSPACE ).parent
     project_workspace_name = script.env.PROJECT_NAME
