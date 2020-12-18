688d0a28be7f733172b03d75ea6bf4fe5c146a43
michael.delva <michael.delva@fishingcactus.com>
2020-12-18 10:43:07 +0100

    Removed github web api util functions

--- src/unreal/utils.groovy
+++ src/unreal/utils.groovy
@@ -62,55 +62,12 @@ def initializeEnvironment(Script script, String project_name_override = null) {
 
     script.env.BRANCH_TYPE = branch_type
     log.info "BranchType : ${script.env.BRANCH_TYPE}"
-    
+
     script.env.DEPLOYMENT_ENVIRONMENT = deployment_environment
     log.info "DeploymentEnvironment ${script.env.DEPLOYMENT_ENVIRONMENT}"
 
     script.env.CLIENT_CONFIG = client_config
     log.info "ClientConfiguration : ${script.env.CLIENT_CONFIG}"
-}
-
-def getGitHubPRInfos( github_token ) {
-    branch_name = env.GIT_BRANCH
-    if (env.BRANCH_NAME != null) {
-        branch_name = env.BRANCH_NAME
-    }
-
-    pr_id = branch_name.substring( 3 )
-    echo "PR ID : ${pr_id}"
-
-    String url = "https://api.github.com/repos/FishingCactus/${env.PROJECT_NAME}/pulls/${pr_id}"
-    
-    def text = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
-    def json = new JsonSlurper().parseText( text )
-
-    log.info "PR Title : ${json.title}"
-
-    return [ json.title, json.body, json.user.login, json.user.avatar_url ]
-}
-
-def getGitHubPRLastCommitterEmail( github_token ) {
-    branch_name = env.GIT_BRANCH
-    if (env.BRANCH_NAME != null) {
-        branch_name = env.BRANCH_NAME
-    }
-
-    pr_id = branch_name.substring( 3 )
-
-    String url = "https://api.github.com/repos/FishingCactus/${env.PROJECT_NAME}/pulls/${pr_id}/commits"
-    
-    def text = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
-    def json = new JsonSlurper().parseText( text )
-    def commits_count = json.size()
-    def last_commit = json[ commits_count - 1 ]
-
-    log.info "Last commit SHA : ${last_commit.sha}"
-
-    def last_commit_author_email = last_commit.commit.author.email
-
-    log.info "Last commit author email : ${last_commit_author_email}"
-
-    return last_commit_author_email
 }
 
 def getProjectName(def script) {
