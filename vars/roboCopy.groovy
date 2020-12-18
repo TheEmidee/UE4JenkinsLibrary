ab1722f41aac625238242868a63c0acaf797068a
michael.delva <michael.delva@fishingcactus.com>
2020-12-18 14:36:12 +0100

    Moved some functions in another jenkins pipeline lib

--- vars/roboCopy.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/groovy
-
-def call( source, destination, arguments ) {
-    def status = bat returnStatus: true, script: "robocopy.exe ${source} ${destination} ${arguments}"
-
-    if (status < 0 || status > 3) {
-        log.fatal( "ROBOCOPY failed" )
-    }
-}
