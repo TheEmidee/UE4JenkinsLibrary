c987f2897d5d5bbf7075d5f359394bb8dbf06690
michael.delva <michael.delva@fishingcactus.com>
2020-12-22 21:21:11 +0100

    runBuildGraph now accepts one task

--- vars/ue4RunBuildGraph.groovy
+++ vars/ue4RunBuildGraph.groovy
@@ -1,14 +1,8 @@
 #!/usr/bin/groovy
 
-def call( ue4_config, List tasks, Map parameters = [:] ) {
+def call( ue4_config, String task, Map parameters = [:] ) {
 
     def UE4 = new unreal.UE4()
     UE4.initialize( ue4_config )
-
-    String all_tasks = ""
-    tasks.each {
-        all_tasks += "${it},"
-    }
-
-    UE4.runBuildGraph( all_tasks, parameters )
+    UE4.runBuildGraph( task, parameters )
 }
