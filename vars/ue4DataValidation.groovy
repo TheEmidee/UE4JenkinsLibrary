5925913ba2f361453def7090c3565c92779e4380
michael.delva <michael.delva@fishingcactus.com>
2020-12-22 21:17:00 +0100

    Use DataValidation.BuildGraphTaskName and DataValidation.Parsers

--- vars/ue4DataValidation.groovy
+++ vars/ue4DataValidation.groovy
@@ -1,27 +1,25 @@
 #!/usr/bin/groovy
 
 def call( ue4_config, buildgraph_params ) {
-    def buildgraph_tasks = []
-    ue4_config.project.DataValidation.each{ task -> 
-        buildgraph_tasks.add( task.BuildGraphTask )
-    }
 
-    if ( ue4_config.project.DataValidation.size() == 0 ) {
-        log.warning "No Data Validation will be done. Did you forget to fill in the DataValidation section of the config file?"
+    def buildgraph_task_name = ue4_config.project.DataValidation.BuildGraphTaskName
+
+    if ( buildgraph_task_name == "" ) {
+        log.warning "No Data Validation will be done. Did you forget to fill in the DataValidation.BuildGraphTaskName section of the config file?"
         return
     }
 
     stage( "Data Validation" ) {
         ue4RunBuildGraph( 
             ue4_config,
-            buildgraph_tasks,
+            buildgraph_task_name,
             buildgraph_params
             )
 
         recordIssues failOnError: true, filters: [excludeCategory('ModuleManager|SwarmsEditor')], qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'NEW', unstable: false]], tools: [msBuild()]
         //recordIssues failOnError: true, sourceCodeEncoding: 'UTF-8', tools: [ msbuild() ], qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'NEW', unstable: false]], filters: [excludeCategory('ModuleManager|SwarmsEditor')]
 
-        ue4_config.project.DataValidation.each{ task -> 
+        ue4_config.project.DataValidation.Parsers.each{ task -> 
             try {
                 timeout(time: 120, unit: 'SECONDS') {
                     def log_file_path = "Saved\\Logs\\${task.LogFileName}.log"
