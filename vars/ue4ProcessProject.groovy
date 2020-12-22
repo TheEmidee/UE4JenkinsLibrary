d8c75d6641244231f8389ca29df5a2a0cf053616
michael.delva <michael.delva@fishingcactus.com>
2020-12-22 18:12:53 +0100

    Typo

--- vars/ue4ProcessProject.groovy
+++ vars/ue4ProcessProject.groovy
@@ -20,5 +20,5 @@ def call( ue4_config ) {
         "OutputDir" : "${env.WORKSPACE}\\${ue4_config.project.RelativeOutputDirectory}\\Win64"
     ]
 
-    ue4DataValidation ue4config
+    ue4DataValidation ue4_config
 }
