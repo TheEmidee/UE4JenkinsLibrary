710a6a3d6f0191c37fd9bd6aaa85db99fae29b1b
michael.delva <michael.delva@fishingcactus.com>
2020-12-23 17:42:35 +0100

    Different archive name for test results

--- vars/ue4RunTests.groovy
+++ vars/ue4RunTests.groovy
@@ -20,7 +20,7 @@ def call( ue4_config, buildgraph_params ) {
         } finally {
             ue4ZipLogs "Tests"
 
-            zip archive: true, dir: "Saved\\Tests\\Logs", glob: '', zipFile: 'Saved\\Tests\\TestsLogs.zip'
+            zip archive: true, dir: "Saved\\Tests\\Logs", glob: '', zipFile: 'Saved\\Tests\\TestsResults_Logs.zip'
             junit testResults: "Saved\\Tests\\Logs\\FunctionalTestsResults.xml"
         }
     }
