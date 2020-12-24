3f6c866d9a910e8e18c376c53e8fddc585206838
michael.delva <michael.delva@fishingcactus.com>
2020-12-24 11:09:42 +0100

    Fix when params are not set

--- vars/ue4InitializeBuildGraphParameters.groovy
+++ vars/ue4InitializeBuildGraphParameters.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     def buildgraph_params = [
-        "Clean" : params.CLEAN_PROJECT,
+        "Clean" : params.CLEAN_PROJECT == null ? false : params.CLEAN_PROJECT,
         "ProjectDir" : env.WORKSPACE,
-        "BuildConfiguration": params.DEBUG_BUILDS ? "Debug" : "Development"
+        "BuildConfiguration": params.DEBUG_BUILDS == null ? "Development" : ( params.DEBUG_BUILDS ? "Debug" : "Development" )
     ]
 
     return buildgraph_params
