8dcf5efa05900458c759c38a5aab91527f8ee5c5
michael.delva <michael.delva@fishingcactus.com>
2020-12-29 15:48:23 +0100

    Updated ue4SyncEngineOnNode

--- vars/ue4SyncEngineOnNode.groovy
+++ vars/ue4SyncEngineOnNode.groovy
@@ -4,9 +4,19 @@ def call( ue4_config ) {
     stage ( "SyncUE" ) {
         log.info "Check if the engine must be synchronized on the node ${env.NODE_NAME}"
 
+        if ( ue4_config.Engine.Version == null || ue4_config.Engine.Version == "" ) {
+            error "You must define the UE4 engine version in the config file"
+            return
+        }
+
+        if ( ue4_config.Engine.ReferenceBuildLocation == null || ue4_config.Engine.ReferenceBuildLocation == "" ) {
+            error "You must define the UE4 ReferenceBuildLocation in the config file"
+            return
+        }
+
         if ( mustSyncUE( ue4_config ) ) {
             log.warning "Must Sync Engine on node ${env.NODE_NAME}"
-            roboCopy( ue4_config.Engine.ReferenceBuildLocation, env.UE4_ROOT_WINDOWS, "/J /NOOFFLOAD /S /R:5 /W:5 /TBD /NP /V /MT:16 /MIR" )
+            // roboCopy( ue4_config.Engine.ReferenceBuildLocation, env.UE4_ROOT_WINDOWS, "/J /NOOFFLOAD /S /R:5 /W:5 /TBD /NP /V /MT:16 /MIR" )
         } else {
             log.info "No need to sync"
         }
@@ -21,28 +31,40 @@ def mustSyncUE( ue4_config ) {
     def saved_jenkins_build_version_reference = "Saved\\${jenkins_build_version_reference}"
     def saved_jenkins_build_version_local = "Saved\\${jenkins_build_version_local}"
 
-    fileOperations([fileDeleteOperation(excludes: '', includes: 'Saved\\JenkinsBuild.*')])
+    fileOperations( [ fileDeleteOperation( excludes: '', includes: 'Saved\\JenkinsBuild.*' ) ] )
 
-    roboCopy( "${ue4_config.Engine.ReferenceBuildLocation}\\Engine\\Build", "${env.WORKSPACE}\\Saved", jenkins_build_version )
+    // First copy from the network share the JenkinsBuild.version file into the Saved folder, and name it JenkinsBuild.version.reference
+    def reference_engine_location = "${ue4_config.Engine.ReferenceBuildLocation}\\${ue4_config.Engine.Version}"
+
+    if ( !roboCopy( reference_engine_location, "${env.WORKSPACE}\\Saved", jenkins_build_version ) ) {
+        error "Failed to copy ${jenkins_build_version} from ${reference_engine_location}"
+        return false
+    }
+
     def exists = fileExists saved_jenkins_build_version
     if ( !exists ) {
         log.warning "Could not find a JenkinsBuild.version file in the network folder"
         return true
     }
-    fileOperations([fileRenameOperation(destination: saved_jenkins_build_version_reference, source: saved_jenkins_build_version)])
-    
-    try {
-        roboCopy( "${env.UE4_ROOT}\\Windows\\Engine\\Build", "${env.WORKSPACE}\\Saved", jenkins_build_version )
-    } catch ( Exception e ) {
+
+    fileOperations( [ fileRenameOperation( destination: saved_jenkins_build_version_reference, source: saved_jenkins_build_version ) ] )
+
+    // Now copy from the engine location on the node the JenkinsBuild.version file into the Saved folder, and name it JenkinsBuild.version.local
+    def local_engine_location = "${env.NODE_UE4_ROOT}\\${ue4_config.Engine.Version}"
+
+    if ( !roboCopy( "${local_engine_location}\\Engine\\Build", "${env.WORKSPACE}\\Saved", jenkins_build_version ) ) {
+        log.warning "Failed to copy ${jenkins_build_version}"
         return true
     }
+
     exists = fileExists saved_jenkins_build_version
 
     if ( !exists ) {
         log.warning "Could not find a JenkinsBuild.version file in the local folder"
         return true
     }
-    fileOperations([fileRenameOperation(destination: saved_jenkins_build_version_local, source: saved_jenkins_build_version )])
+
+    fileOperations( [ fileRenameOperation( destination: saved_jenkins_build_version_local, source: saved_jenkins_build_version ) ] )
 
     def version_reference = readFile encoding: 'utf-8', file: saved_jenkins_build_version_reference
     def version_local = readFile encoding: 'utf-8', file: saved_jenkins_build_version_local
