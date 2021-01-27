0d5ab6577ce02cf65e5df4bc861c4b8a7f5748ec
michael.delva <michael.delva@fishingcactus.com>
2021-01-27 11:40:26 +0100

    Exclude issues categories for data validation scanForIssues

--- vars/ue4DataValidation.groovy
+++ vars/ue4DataValidation.groovy
@@ -18,6 +18,8 @@ def call( ue4_config, buildgraph_params ) {
             buildgraph_params
             )
 
+        def config_excluded_categories = ue4_config.Project.IssuesExcludedCategories.join('|')
+
         ue4_config.Project.DataValidation.Parsers.each { task -> 
             def parser = task.Parser
 
@@ -26,7 +28,7 @@ def call( ue4_config, buildgraph_params ) {
                     def log_file_path = "Saved\\Logs\\${parser.LogFileName}.log"
 
                     // recordIssues sometimes times out so first scan then publish
-                    def data_validation_issues = scanForIssues blameDisabled: true, forensicsDisabled: true, tool: groovyScript(parserId: "${parser.ParserName}", pattern: "${log_file_path}", reportEncoding: 'UTF-8')
+                    def data_validation_issues = scanForIssues blameDisabled: true, forensicsDisabled: true, tool: groovyScript(parserId: "${parser.ParserName}", pattern: "${log_file_path}", reportEncoding: 'UTF-8'), filters: [excludeCategory( config_excluded_categories )]
                     publishIssues failOnError: true, qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true]], issues: [ data_validation_issues ]
                 }
             } catch ( e ) {
