f3f58b663a02ff8d2a0abf56a44bffaadf01c6c8
michael.delva <michael.delva@fishingcactus.com>
2021-09-13 13:10:49 +0200

    Created ue4GitCommit task

--- vars/ue4BuildContent.groovy
+++ vars/ue4BuildContent.groovy
@@ -32,38 +32,13 @@ def call( ue4_config, buildgraph_params ) {
         ue4ParseLogs( ue4_config.Project.BuildContent.LogParsers )
 
         if ( ue4_config.Project.BuildContent.CanCommitContent ) {
-            def ssh_credentials = ue4_config.Git.SSHAgentCredentials
-            def git_username = ue4_config.Git.UserName
-            def git_email = ue4_config.Git.Email
+            ue4GitCommit(
+                ue4_config, 
+                "Built Content"
+            )
 
-            if ( !ssh_credentials?.trim() ) {
-                log.warning "Can not commit content because the option Git.SSHAgentCredentials is not set"
-                return
-            }
-
-            if ( !git_username?.trim() ) {
-                log.warning "Can not commit content because the option Git.UserName is not set"
-                return
-            }
-
-            if ( !git_email?.trim() ) {
-                log.warning "Can not commit content because the option Git.Email is not set"
-                return
-            }
-
-            def git_branch = env.GIT_BRANCH
-            def origin_str = "origin/"
-            if ( git_branch.startsWith( origin_str ) ) {
-                git_branch = git_branch.substring( origin_str.length() )
-            }
-
-            sshagent( [ ssh_credentials ] ) {
-                bat "git config user.email ${git_email}"
-                bat "git config user.name ${git_username}"
-                bat "git switch -C ${git_branch} HEAD"
-                bat "git commit -am \"Built Content\" -n"
-
-                if ( ue4_config.Project.BuildContent.CanPushContent ) {
+            if ( ue4_config.Project.BuildContent.CanPushContent ) {
+                sshagent( [ ssh_credentials ] ) {
                     bat "git push --set-upstream origin ${git_branch}"
                 }
             }
