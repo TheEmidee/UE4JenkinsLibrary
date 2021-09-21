d1dc4d0441d8d87b3c7564cece7eaaf8f40eaf4d
michael.delva <michael.delva@fishingcactus.com>
2021-09-21 22:12:56 +0200

    Removed excludedCategories from ue4ParseLogs

--- vars/ue4ParseLogs.groovy
+++ vars/ue4ParseLogs.groovy
@@ -19,7 +19,7 @@ def call( parsers ) {
                 }
 
                 // recordIssues sometimes times out so first scan then publish
-                def issues = scanForIssues blameDisabled: true, forensicsDisabled: true, tool: groovyScript(parserId: "${parser.ParserName}", name: "${custom_name}", pattern: "${log_file_path}", reportEncoding: 'UTF-8'), filters: [excludeCategory( excluded_categories )]
+                def issues = scanForIssues blameDisabled: true, forensicsDisabled: true, tool: groovyScript(parserId: "${parser.ParserName}", name: "${custom_name}", pattern: "${log_file_path}", reportEncoding: 'UTF-8')
                 publishIssues failOnError: true, qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true]], issues: [ issues ]
             }
         } catch ( e ) {
