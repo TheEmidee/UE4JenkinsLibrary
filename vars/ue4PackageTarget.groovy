eef67618769c879ea77695d148b952780d829f35
michael.delva <michael.delva@fishingcactus.com>
2021-12-21 12:07:44 +0100

    Added a pattern for msBuild recordIssues in ue4PackageTarget

--- vars/ue4PackageTarget.groovy
+++ vars/ue4PackageTarget.groovy
@@ -35,7 +35,7 @@ def call( String type, String platform, ue4_config, buildgraph_params ) {
 
         if ( ue4_config.Project.Package.RecordIssues ) {
             recordIssues tools: [groovyScript(id: "BuildCookRun_${type}_${platform}", name: "BuildCookRun_${type}_${platform}", parserId: 'UE4_BuildCookRun', pattern: 'Saved/Logs/Log.txt')], failOnError: true, filters: [ excludeCategory('LogTemp|ModuleManager|SwarmsEditor') ], qualityGates: [[threshold: 1, type: 'TOTAL', unstable: true]]
-            recordIssues tools: [msBuild(id: "MSBuild_${type}_${platform}", name: "MSBuild_${type}_${platform}")], failOnError: true, filters: [excludeCategory( 'LogTemp|ModuleManager|SwarmsEditor' )], qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'NEW', unstable: false]]
+            recordIssues tools: [msBuild(id: "MSBuild_${type}_${platform}", name: "MSBuild_${type}_${platform}", pattern: 'Saved/Logs/UBT-*.txt')], failOnError: true, filters: [excludeCategory( 'LogTemp|ModuleManager|SwarmsEditor' )], qualityGates: [[threshold: 1, type: 'TOTAL_ERROR', unstable: false], [threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'NEW', unstable: false]]
         }
 
         ue4ZipLogs zip_file_name
