3a23dccb75250f2479d948bce38fbc7b8ef64863
michael.delva <michael.delva@fishingcactus.com>
2022-05-13 07:15:00 +0200

    runBuildGraph creates a stage

--- vars/ue4RunBuildGraph.groovy
+++ vars/ue4RunBuildGraph.groovy
@@ -1,17 +1,18 @@
 #!/usr/bin/groovy
 
 def call( ue4_config, String task, Map parameters = [:] ) {
+    stage( task ) {
+        if ( ue4_config.Options.Stub ) {
+            echo "Would run build graph task ${task} with parameters ${parameters}"
+            return
+        }
 
-    if ( ue4_config.Options.Stub ) {
-        echo "Would run build graph task ${task} with parameters ${parameters}"
-        return
+        fileOperations( [ 
+            fileDeleteOperation( excludes: '', includes: 'Saved\\Logs\\*.*' )
+        ] )
+
+        def UE4 = new unreal.UE4()
+        UE4.initialize( ue4_config )
+        UE4.runBuildGraph( task, parameters )
     }
-
-    fileOperations( [ 
-        fileDeleteOperation( excludes: '', includes: 'Saved\\Logs\\*.*' )
-    ] )
-
-    def UE4 = new unreal.UE4()
-    UE4.initialize( ue4_config )
-    UE4.runBuildGraph( task, parameters )
 }
