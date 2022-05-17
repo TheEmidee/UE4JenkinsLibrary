5a88be38d22e557df10e3536bb010fd49772893d
michael.delva <michael.delva@fishingcactus.com>
2022-05-17 16:11:36 +0200

    Removed stage from ueRunBuildGraph

--- vars/ue4RunBuildGraph.groovy
+++ vars/ue4RunBuildGraph.groovy
@@ -1,18 +1,16 @@
 #!/usr/bin/groovy
 
 def call( ue4_config, String task, Map parameters = [:] ) {
-    stage( task ) {
-        if ( ue4_config.Options.Stub ) {
-            echo "Would run build graph task ${task} with parameters ${parameters}"
-            return
-        }
-
-        fileOperations( [ 
-            fileDeleteOperation( excludes: '', includes: 'Saved\\Logs\\*.*' )
-        ] )
-
-        def UE4 = new unreal.UE4()
-        UE4.initialize( ue4_config )
-        UE4.runBuildGraph( task, parameters )
+    if ( ue4_config.Options.Stub ) {
+        echo "Would run build graph task ${task} with parameters ${parameters}"
+        return
     }
+
+    fileOperations( [ 
+        fileDeleteOperation( excludes: '', includes: 'Saved\\Logs\\*.*' )
+    ] )
+
+    def UE4 = new unreal.UE4()
+    UE4.initialize( ue4_config )
+    UE4.runBuildGraph( task, parameters )
 }
